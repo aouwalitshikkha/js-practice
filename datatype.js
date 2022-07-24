cedeb3d920511781a9d36fd551526bb9b036931a
Abdul Aouwal <aouwalitshikkha@gmail.com>
2022-07-24 17:03:46 +0600

    Data Type file added

---
+++ datatype.js
@@ -0,0 +1,29 @@
+// String data types 
+var myName = "Abdul Aouwal"
+// number Data Types 
+var age = 25
+
+// Boolean Data Types
+var human = true
+var female = false
+
+// Other Data types
+var x   // it is undefined
+var y = null
+
+
+
+
+
+// Main Classification
+
+// --- Primitive Data Types
+// ------ Number
+// ------ String
+
+// Object Type Data Types
+// ----- Array
+// ----- functions
+// ----- objects
+
+
--- readme.md
+++ readme.md
@@ -4,3 +4,4 @@ Chapter Wise Practice and codes
 1. print.js  - Practicing Console log and Printing data
 2. variable.js - Working with Variable 
 3. reserved-word.js - List of JavaScript Reserved Keywords
+4. datatype.js - Idea about JS Data Types
