98dbc7a43f8789242f1358bf1f7815f6a8017549
Abdul Aouwal <aouwalitshikkha@gmail.com>
2022-07-24 16:54:49 +0600

    Reserved Word List

---
+++ module_one/researved-word.js
@@ -0,0 +1,31 @@
+//  According to the convention and rules 
+// Reserved word can't be used in variable name 
+
+// reserved wordlist can be found in following link
+
+// https://www.w3schools.com/js/js_reserved.asp
+
+// List Copied from above link
+
+// ========================================
+
+
+// abstract	arguments	await*	boolean
+// break	byte	case	catch
+// char	class*	const	continue
+// debugger	default	delete	do
+// double	else	enum*	eval
+// export*	extends*	false	final
+// finally	float	for	function
+// goto	if	implements	import*
+// in	instanceof	int	interface
+// let*	long	native	new
+// null	package	private	protected
+// public	return	short	static
+// super*	switch	synchronized	this
+// throw	throws	transient	true
+// try	typeof	var	void
+// volatile	while	with	yield
+
+
+// The above keywords are used in different js functions
--- readme.md
+++ readme.md
@@ -3,3 +3,4 @@ Chapter Wise Practice and codes
 ## Chapter One 
 1. print.js  - Practicing Console log and Printing data
 2. variable.js - Working with Variable 
+3. reserved-word.js - List of JavaScript Reserved Keywords
