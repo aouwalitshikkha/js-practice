8e1348b806aaca8f355bc9184be22c80f906669c
Abdul Aouwal <aouwalitshikkha@gmail.com>
2022-07-24 18:59:58 +0600

    if Condition is added

---
+++ module_two/if.js
@@ -0,0 +1,43 @@
+var a = 10
+var b = 20
+
+// Print Which is larger
+
+/*
+Conditional Operators
+a > b  or b >a
+*/
+
+// Skeleton
+// ====================
+
+/*
+
+if (condition) {
+    // Code will be executed 
+}
+
+*/
+
+if (a > b) {
+    console.log(a + 'is greater than ' + b)
+}
+
+if (a < b) {
+    console.log(b + ' is Greater than ' + a)
+}
+
+
+// Find the odd or Even Number with If Condition 
+
+var a = 11
+
+if (a % 2 == 0) {
+    console.log(a + " is even Number")
+}
+
+
+if (a % 2 == 1) {
+    console.log(a + " is odd Number")
+}
+
