5149406c7a3be47b372235a74f65dcd042fecfcc
Abdul Aouwal <aouwalitshikkha@gmail.com>
2022-07-24 16:46:55 +0600

    Working with variable

---
+++ module_one/variable.js
@@ -0,0 +1,11 @@
+var myName = "Abdul Aouwal"
+var age = 32
+// Printing Variable only
+
+console.log(myName);
+console.log(age);
+
+// Printing Variable with Data
+
+console.log("This is " + myName)
+console.log("I am " + age + " years old")
--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@
 Chapter Wise Practice and codes
 ## Chapter One 
 1. print.js  - Practicing Console log and Printing data
+2. variable.js - Working with Variable 
